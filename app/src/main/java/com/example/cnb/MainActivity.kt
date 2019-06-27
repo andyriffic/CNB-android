5e20690135068b17d09b9b8caa17a4d80f4623de
FooDog <andyriffic@gmail.com>
2019-06-27 20:54:44 +1000

    Add play button behind 'feature' toggle

--- app/src/main/java/com/example/cnb/MainActivity.kt
+++ app/src/main/java/com/example/cnb/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.example.cnb
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var _activateDevModeCount: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
+        val devMode = sharedPref.getBoolean("DEV_MODE", false)
+
+        if (!devMode) {
+            playButton.visibility = View.GONE
+        }
+
+        titleTextButton.setOnClickListener {
+            this._activateDevModeCount += 1
+
+            if (this._activateDevModeCount % 5 == 0) {
+                val newDevMode = !sharedPref.getBoolean("DEV_MODE", false)
+                sharedPref.edit().putBoolean("DEV_MODE", newDevMode).commit()
+                playButton.visibility = if (newDevMode) {
+                    View.VISIBLE
+                } else {
+                    View.GONE
+                }
+            }
+        }
 
         watchButton.setOnClickListener {
             val intent = Intent(this, Spectator::class.java)
